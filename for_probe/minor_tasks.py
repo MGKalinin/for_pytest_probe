3fe2d11d646432b2dd843072ccfe6702d2cbc369
MG_Kalinin <1953115@gmail.com>
2025-11-09 00:56:56 +0300

    reduct README.md

--- for_probe/minor_tasks.py
+++ for_probe/minor_tasks.py
@@ -1,33 +1,47 @@
 import requests
-import pytest
 
-# pip install pytest - mock
-
-def get_post_title(post_id: int) -> str:
-    """Возвращает заголовок поста по его ID."""
-    response = requests.get(f"https://jsonplaceholder.typicode.com/posts/{post_id}")
+base_url="https://jsonplaceholder.typicode.com/posts"
+def get_post_titles_by_user_id(url:str,user_id: int) -> list[str]:
+    response = requests.get(url)
     response.raise_for_status()
-    return response.json()["title"]
+    response_json=response.json()
+    answer=[]
+    for post in response_json:
+        if post["userId"] == user_id:
+            answer.append(post["title"])
+    return answer
+# print(get_post_titles_by_user_id(base_url,1))
 
 
-def test_get_post_title(mocker):
-    # Подготовка фейкового ответа (один объект, не список!)
-    mock_response_data = {
-        "userId": 1,
-        "id": 42,
-        "title": "My custom mocked title",
-        "body": "..."
-    }
-
-    # Мокаем requests.get
-    mock_get = mocker.patch("requests.get")
-    mock_get.return_value.status_code = 200
+def test_get_post_titles_by_user_id(mocker):
+    mock_response_data = [
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body": "quia et suscipit..."
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "qui est esse",
+            "body": "est rerum tempore..."
+        },
+        {
+            "userId": 2,  # пост другого пользователя - должен быть отфильтрован
+            "id": 3,
+            "title": "other user post",
+            "body": "..."
+        }
+    ]
+    mock_get=mocker.patch("requests.get")
+    mock_get.return_value.status_code=200
     mock_get.return_value.json.return_value = mock_response_data
 
-    # Вызываем функцию
-    result = get_post_title(42)
-
-    # Проверки
-    assert result == "My custom mocked title"
-    mock_get.assert_called_once_with("https://jsonplaceholder.typicode.com/posts/42")
+    result=get_post_titles_by_user_id(base_url,1)
+    expected_titles = [
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "qui est esse"
+    ]
+    assert result == expected_titles
 
